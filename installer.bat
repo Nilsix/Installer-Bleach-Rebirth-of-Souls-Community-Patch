5c4933e2d40797b61f341723c82ff6ea081b6211
Nilsix <a.nilsix20@gmail.com>
2026-05-31 04:51:36 +0200

    Installer

---
+++ installer.bat
@@ -0,0 +1,6 @@
+@echo off 
+winget install --id Python.Python.3.14 -e --accept-source-agreements --accept-package-agreements
+winget install --id Git.Git -e --accept-source-agreements --accept-package-agreements
+
+git clone https://github.com/Nilsix/Bleach-Rebirth-of-Souls-Community-Patch.git
+pause
