aa93e5e2c304a99e1530b1ccde65bbe954ccc617
Nilsix <a.nilsix20@gmail.com>
2026-05-31 05:00:29 +0200

    added Mac support

---
+++ InstallerMac.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "Installing dependencies..."
+
+
+if ! command -v python3 &> /dev/null; then
+    echo "Python not found, installing via Homebrew..."
+    brew install python
+fi
+
+if ! command -v git &> /dev/null; then
+    echo "Installing Git..."
+    brew install git
+fi
+
+echo "Cloning repo..."
+git clone https://github.com/Nilsix/Bleach-Rebirth-of-Souls-Community-Patch.git
+
+read -p "Press Enter to exit..."
