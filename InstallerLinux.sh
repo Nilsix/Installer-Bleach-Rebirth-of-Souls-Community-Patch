f84481a9d4dedadd510db17bc7cd274ba1934fc2
Nilsix <a.nilsix20@gmail.com>
2026-05-31 04:55:33 +0200

    added Linux support

---
+++ InstallerLinux.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Updating packages..."
+sudo apt update
+
+echo "Installing Python..."
+sudo apt install -y python3 python3-pip
+
+echo "Installing Git..."
+sudo apt install -y git
+
+echo "Cloning repo..."
+git clone https://github.com/Nilsix/Bleach-Rebirth-of-Souls-Community-Patch.git
+
+echo "Done."
+read -p "Press enter to exit..."
