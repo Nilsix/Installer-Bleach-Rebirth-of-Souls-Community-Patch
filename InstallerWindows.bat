78d187f505fc36b5d75cefa295257fc87c0a355a
Nilsix <a.nilsix20@gmail.com>
2026-06-05 17:47:39 +0200

    message

--- InstallerWindows.bat
+++ InstallerWindows.bat
@@ -1,5 +1,5 @@
 @echo off 
-echo Press Enter to start the installation, after the installation is done, the window will close itself, DO NOT close it yourself or else this would break the launcher
+echo After the installation is done, the window will close itself, DO NOT close it yourself or else this would break the launcher
 pause
 winget install --id Python.Python.3.14 -e --accept-source-agreements --accept-package-agreements
 winget install --id Git.Git -e --accept-source-agreements --accept-package-agreements*
