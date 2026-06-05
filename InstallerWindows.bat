2d50da1ed5cc29edc5315cad9bcdd76dc8584db3
Nilsix <a.nilsix20@gmail.com>
2026-06-05 17:46:51 +0200

    message

--- InstallerWindows.bat
+++ InstallerWindows.bat
@@ -1,6 +1,7 @@
 @echo off 
+echo Press Enter to start the installation, after the installation is done, the window will close itself, DO NOT close it yourself or else this would break the launcher
+pause
 winget install --id Python.Python.3.14 -e --accept-source-agreements --accept-package-agreements
-winget install --id Git.Git -e --accept-source-agreements --accept-package-agreements
-
+winget install --id Git.Git -e --accept-source-agreements --accept-package-agreements*
 git clone https://github.com/Nilsix/Bleach-Rebirth-of-Souls-Community-Patch.git
 
