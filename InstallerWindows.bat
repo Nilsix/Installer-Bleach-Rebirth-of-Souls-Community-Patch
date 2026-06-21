4e9a0f830fcc98bf10a8c22d9dc311facf53a8c5
Nilsix <a.nilsix20@gmail.com>
2026-06-21 14:12:24 +0200

    fix

--- InstallerWindows.bat
+++ InstallerWindows.bat
@@ -5,15 +5,6 @@ pause
 winget install --id Python.Python.3.11 -e --accept-source-agreements --accept-package-agreements
 
 winget install --id Git.Git -e --accept-source-agreements --accept-package-agreements
-:wait_python
-py -0 | find "3.11" >nul
-if errorlevel 1 (
-    timeout /t 2 >nul
-    goto wait_python
-)
-
-py -3.11 -m pip install --upgrade pip
-py -3.11 -m pip install pygame
 
 if exist Bleach-Rebirth-of-Souls-Community-Patch (
 	rmdir /s /q Bleach-Rebirth-of-Souls-Community-Patch
